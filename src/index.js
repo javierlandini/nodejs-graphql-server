6dedee387faf811de9806499e7e928216570832c
Javier Landini <Javier.Landini@wwecorp.com>
2021-04-12 16:58:58 -0300

    Add mutation.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const path = require('path')
 
 const links = [{
-    id: 'link-1',
+    id: 'link-0',
     description: 'First link',
     url: 'https://www.example1.com',
 }]
@@ -12,6 +12,17 @@ const resolvers = {
     Query: {
         info: () => 'This is a sample API cloning Hackernews',
         feed: () => links,
+    },
+    Mutation: {
+        createLink: (parent, args) => {
+            const link = {
+                id: `link-${links.length}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link)
+            return link
+        }
     },
     Link: {
         description: (parent) => parent.description.replace(/[a|e|i|o|u]/ig, '')

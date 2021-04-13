9f35dd856c293d4f4f341323556335b34bb10a38
Javier Landini <Javier.Landini@wwecorp.com>
2021-04-13 08:46:06 -0300

    Add update and delete mutations.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { ApolloServer } = require('apollo-server')
 const fs = require('fs')
 const path = require('path')
 
-const links = [{
+let links = [{
     id: 'link-0',
     description: 'First link',
     url: 'https://www.example1.com',
@@ -22,6 +22,32 @@ const resolvers = {
             }
             links.push(link)
             return link
+        },
+        updateLink: (parent, args) => {
+            let linksMap = new Map(links.map((o) => [o.id, o]));
+            let link = linksMap.get(args.id);
+            if (link) {
+                if (args.url &&
+                    args.url !== link.url) {
+                    link.url = args.url;
+                }
+                if (args.description &&
+                    args.description !== link.description) {
+                    link.description = args.description;
+                }
+                linksMap.set(args.id, link);
+                links = Array.from(linksMap.values());
+                return link;
+            }
+        },
+        deleteLink: (parent, args) => {
+            let linksMap = new Map(links.map((l) => [l.id, l]));
+            const link = linksMap.get(args.id);
+            if (link) {
+                linksMap.delete(args.id);
+                links = Array.from(linksMap.values());
+                return link
+            }
         }
     },
     Link: {
